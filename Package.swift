5ad88f50967a803839699afb25c2da0144a464ba
madhan-tonetag <madhan.kumar@tonetag.com>
2025-09-18 17:42:12 +0530

    update fastag flutter framework

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Flutter",
-            url:"https://madhanrkvaws.s3.eu-north-1.amazonaws.com/Flutter335.xcframework.zip",
+            url:"https://fastag-ios-sdk.s3.ap-south-1.amazonaws.com/Flutter335.xcframework.zip",
             checksum: "86961f5c203419fece22012419cc1278d3cb15d73469ac2a004aadb567dcd1e3"
         )       
         
