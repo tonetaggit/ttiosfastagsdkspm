f08b12cd8ce4f6ba8300d45ff181604ff60404a9
madhan-tonetag <madhan.kumar@tonetag.com>
2025-11-22 18:05:05 +0530

    update appframework Name

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         ),        
         .binaryTarget(
             name: "fastagiosapp",
-            path: "./App.xcframework"
+            path: "./FastagIos.xcframework"
         ),
         
     ]
